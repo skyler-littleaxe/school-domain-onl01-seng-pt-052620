02289d39ffecda21495c43578e7b65bb4b438856
Skyler Littleaxe <littleaxe.skyler@gmail.com>
2020-06-15 00:42:39 +0000

    Automatically backed up by Learn

--- lib/school.rb
+++ lib/school.rb
@@ -11,4 +11,16 @@ class School
     roster[grade] ||= []
     roster[grade] << student_name
   end  
+  
+  def grade (student_grade)
+    roster[student_grade]
+  end  
+  
+  def sort
+    sorted = {}
+    roster.each do |grade, students|
+      sorted[grade] = students.sort
+    end
+    sorted
+  end
 end  
